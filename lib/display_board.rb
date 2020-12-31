9a518b9cf4e1c0be6a77d56307bd7c6382bced60
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-31 06:27:23 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -11,6 +11,7 @@ def display_board(board)
   puts "-----------"
   puts "#{board[6]}  |#{board[7]}  |#{board[8]}  "
 end
+display_board(board)
   
   def display_board(board)
     board = [" "," "," "," ","X"," "," "," "," "]
