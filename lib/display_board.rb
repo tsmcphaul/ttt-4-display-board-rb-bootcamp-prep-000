b597b74f51d5c83fc96077f67013f240e8e3d7d9
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-31 06:42:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -7,9 +7,9 @@ def display_board(board)
 separator = "|"
 
 
-puts " #{board[0]} | #{board[1]} #{separator} #{board[2]} "
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
-puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
-puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
