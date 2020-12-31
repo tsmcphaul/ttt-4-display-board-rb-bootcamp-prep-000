d789cafc712d7e630567b658ddc108c8eb4bccd8
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-31 06:37:41 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,23 +2,14 @@
 # out the current state.
 
 
-
-def display_board(board)
-  board = [" "," "," "," "," "," "," "," "," "]
-  puts "#{board[0]}  |#{board[1]}  |#{board[2]}  "
-  puts "-----------"
-  puts "#{board[3]}  |#{board[4]}  |#{board[5]}  "
-  puts "-----------"
-  puts "#{board[6]}  |#{board[7]}  |#{board[8]}  "
-end
-
-def display_board(board)
-  board = [" "," "," "," "," X "," "," "," "," "]
-  puts "#{board[0]}  |#{board[1]}  |#{board[2]}  "
-  puts "-----------"
-  puts "#{board[3]}  |#{board[4]}  |#{board[5]}  "
-  puts "-----------"
-  puts "#{board[6]}  |#{board[7]}  |#{board[8]}  "
-end
   
-  
+def display_board(board)
+separator = "|"
+lines = "-----------"
+
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
+end
