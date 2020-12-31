4069503baaa55f1628d9673e3e404356e9eb9690
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-31 06:39:46 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,7 +8,7 @@ separator = "|"
 lines = "-----------"
 
 puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
-puts "#{lines}"
+puts "-----------"
 puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
 puts "#{lines}"
 puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
