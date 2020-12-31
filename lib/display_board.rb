e5fd1389018e5a92b051035698eaebad98db596e
proactive-callee-1012 <shaemc22@gmail.com>
2020-12-31 05:50:47 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,4 +1,12 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-puts "   "
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  
+  
+end  
